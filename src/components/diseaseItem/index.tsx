0adc52129252f056ddb2001482b145d6c51e8496
solchan98 <dev.sol0427@gmail.com>
2022-05-20 16:10:42 +0900

    feat: 검색 결과에 검색단어 하이라이트 적용 구현

--- src/components/diseaseItem/index.tsx
+++ src/components/diseaseItem/index.tsx
@@ -2,6 +2,8 @@ import styles from './diseaseItem.module.scss';
 import { BsSearch } from 'react-icons/bs';
 import cn from 'classnames';
 import { IDiseaseItem } from 'types/disease';
+import { useRecoilValue } from 'recoil';
+import { searchWordState } from '../../states/disease';
 
 const cx = cn.bind(styles);
 
@@ -13,10 +15,15 @@ interface Props {
 
 const DiseaseItem = ({ disease, nameIdx, idx }: Props) => {
   const { sickNm } = disease;
+  const searchWord = useRecoilValue(searchWordState);
   return (
     <li className={styles.diseaseWrapper}>
       <BsSearch className={styles.reactIcons} />
-      <span className={cx(styles.searchWord, { [styles.highlight]: nameIdx === idx })}>{sickNm}</span>
+      <span className={cx(styles.searchWord, { [styles.highlight]: nameIdx === idx })}>
+        {sickNm.substring(0, sickNm.indexOf(searchWord))}
+        <mark>{searchWord}</mark>
+        {sickNm.substring(sickNm.indexOf(searchWord) + searchWord.length)}
+      </span>
     </li>
   );
 };
