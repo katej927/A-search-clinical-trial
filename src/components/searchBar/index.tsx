b0f1db739990c8a4048c35e518d666b84d47a947
daseuls <dls2tmfs2@naver.com>
2022-05-20 10:31:27 +0900

    fix: 에러 해결

--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -14,8 +14,6 @@ const SearchBar = () => {
 
   const ref = useRef<HTMLUListElement | null>(null);
 
-  // const debouncedValue = useDebounce(searchText); // lodash 로 수정 / 주석처리 일단 했습니다.
-
   const { isLoading, data: searchResult } = useQuery(
     ['getDiseaseName', searchText],
     () => getSearchResult(searchText),
@@ -24,7 +22,6 @@ const SearchBar = () => {
       refetchOnWindowFocus: false,
       staleTime: 6 * 10 * 1000,
       cacheTime: Infinity,
-      // keepPreviousData: true, 이 부분이 true면 로딩이 계속 false로 나와서 일단 주석 처리 했습니다.
     }
   );
 
